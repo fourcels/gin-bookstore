f0eb91c6a95a61a3906de69cb3bf52c08ea2ef6b
Kiyan <fourcels@gmail.com>
2023-03-03 14:12:44 +0800

    log

--- models/setup.go
+++ models/setup.go
@@ -5,16 +5,13 @@ import (
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 var DB *gorm.DB
 
 func ConnectDatabase() {
 
-	database, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{
-		Logger: logger.Default.LogMode(logger.Info),
-	})
+	database, err := gorm.Open(postgres.Open(os.Getenv("DATABASE_URL")), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to connect to database!")
